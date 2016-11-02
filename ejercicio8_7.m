ee540242aabc570ab8a0636f471b43e31b30ee84
AndresSanchezR12 <andres.sanchezr@autonoma.edu.co>
2016-11-02 16:49:44 -0500

    Create ejercicio8_7.m

---
+++ ejercicio8_7.m
@@ -0,0 +1,19 @@
+close all
+clc
+clear all
+A=input('capital=')
+k=input('numero de años=')
+r=input('rata anual=')
+n=input('dijite la frecuiencia del periodo "12" para mensual,"1"anual,"360"diario')
+if n==12
+    y=1:(12*k);
+    V=A*(1+(r./y)).^(y*k)
+end
+ if n==1
+    x=1:(k*n);
+    V=A*(1+(r./x)).^(x*k)
+ end
+ if n==360
+     z=1:(k*n);
+     V=A*(1+(r./z)).^(z*k)
+ end
